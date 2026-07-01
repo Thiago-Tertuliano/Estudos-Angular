6acc986a889bc183ba7a17ed832b95454aebcdea
Thiago Matos Tertuliano <thiagomatostertuliano@gmail.com>
2026-07-01 14:03:36 -0300

    Sprint 0.0 e Sprint 0.1, Estrutura main.ts, index.html & first feature

---
+++ Projetos/Restaurant-Front/src/app/app.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `<h1>Restaurant Front</h1><p>Sprint 0 - app vivo</p>`,
+  styleUrl: './app.scss',
+})
+export class App {}
---
+++ Projetos/Restaurant-Front/src/app/features/home/pages/home.page.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  template: `
+    <h1>Restaurant Front</h1>
+    <p>Sprint 0.2 - roteamento funcionando!</p>
+    <p>Bem-vindo ao painel do restaurante!</p>
+  `,
+})
+export class HomePage {}
